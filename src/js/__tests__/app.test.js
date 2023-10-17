73f9950a4ccfbdaa82ceca947ddf012d0eb34519
Wayne <meibeat@mail.ru>
2023-10-17 16:25:51 +0300

    100% tests coverage

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -6,6 +6,10 @@ test.each([
   ['wounded', 49, { name: 'Маг', health: 49 }],
   ['wounded', 15, { name: 'Маг', health: 15 }],
   ['critical', 14, { name: 'Маг', health: 14 }],
+  ['dead', 0, { name: 'Маг', health: 0 }],
+  ['dead', -10, { name: 'Маг', health: -10 }],
+  ['dead', null, { name: 'Маг', health: null }],
+  ['dead', undefined, { name: 'Маг', health: undefined }],
 ])('app testing for %s status and %i health amount', (status, _, object) => {
   const result = getHealthStatus(object);
   expect(result).toBe(status);
--- src/js/app.js
+++ src/js/app.js
@@ -5,21 +5,22 @@ console.log('worked');
 
 console.log(sum([1, 2]));
 
-const dataObj = {
-  name: 'Маг',
-  health: 90,
-};
-
 export default function getHealthStatus(data) {
   if (data.health >= 50) {
     return 'healthy';
-  };
+  }
 
   if (data.health < 50 && data.health >= 15) {
     return 'wounded';
-  };
+  }
 
-  if (data.health < 15) {
+  if (data.health < 15 && data.health >= 1) {
     return 'critical';
-  };
-};
+  }
+
+  if (data.health === 0 || data.health < 0) {
+    return 'dead';
+  }
+
+  return 'dead';
+}
